1ab484b312eea7680f038c42fbf26539d7404345
aysegul.arpacik <aysegul.arpacikk@gmail.com>
2025-12-03 23:23:33 +0300

    added vector algorithm

---
+++ src/Book/VectorNumber.java
@@ -0,0 +1,30 @@
+package Book;
+
+import java.util.Vector;
+
+public class VectorNumber {
+
+    public static void main(String[] args) {
+        Vector<Integer> numbers = new Vector<>();
+        int A[] = new int[numbers.capacity()];
+        System.out.print("Vector = ");
+
+        for(int i=0; i<numbers.capacity(); i++) {
+            numbers.add(1 + (int)(Math.random() * 90));
+            System.out.print(numbers.get(i) + " ");
+            A[i] = numbers.get(i);
+        }
+
+        System.out.println();
+
+        int min = A[0];
+
+        for(int i=0; i<A.length; i++) {
+            if (A[i] < min) {
+                min = A[i];
+            }
+        }
+
+        System.out.println("En küçük sayı = " + min);
+    }
+}
